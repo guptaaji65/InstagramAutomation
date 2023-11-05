2db7c9a21eb96d7428fb373fe1447d75056bd817
guptaaji65 <132967424+guptaaji65@users.noreply.github.com>
2023-11-05 09:20:35 +0530

    bot_followusers.py

---
+++ bot_followusers.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append("instabot");
+
+from instabot import Bot
+from time import sleep
+import random
+from datetime import datetime
+
+accounts = [];
+accCount = 0;
+
+#Fill this in with your info
+myAccount = "aadityaaguptaaa";
+myPassword = "hahahahawontdisclose";
+
+ #fill this out with a starting page
+accounts.append("aadityaaguptaaa");
+
+bot = Bot(filter_users = True, max_follows_per_day = 3840, max_unfollows_per_day = 3840);
+
+bot.login(username = myAccount, password = myPassword	);
+
+for acc in accounts:
+
+	followers = bot.get_user_followers(acc);
+
+	skipped = bot.skipped_file;
+	followed = bot.followed_file;
+	unfollowed = bot.unfollowed_file;
+
+	followers = list(set(followers) - skipped.set - followed.set - unfollowed.set);
+	followCount = 0;
+
+	bot.follow_users(followers);
+		
+					
+	accCount += 1;
